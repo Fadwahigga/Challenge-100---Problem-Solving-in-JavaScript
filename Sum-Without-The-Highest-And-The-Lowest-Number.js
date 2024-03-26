0915fbb52fa85abcb2de9bb752706a7ee98042b1
Fadwa Ali <fadwa.ali@elevoro.com>
2024-03-26 15:45:32 +0300

    SumWithoutTheHighestAndTheLowestNumber2

--- Sum-Without-The-Highest-And-The-Lowest-Number.js
+++ Sum-Without-The-Highest-And-The-Lowest-Number.js
@@ -7,4 +7,11 @@ function SumWithoutTheHighestAndTheLowestNumber(Array) {
   return newArray;
 }
 SumWithoutTheHighestAndTheLowestNumber([1, 3, 5, 9]);
-console.log(SumWithoutTheHighestAndTheLowestNumber([1, 3, 5, 9]))
+console.log(SumWithoutTheHighestAndTheLowestNumber([1, 3, 5, 9]));
+function SumWithoutTheHighestAndTheLowestNumber2(Array) {
+  if (Array == null) return 0;
+
+  return Array.sort((a,b)=> a-b).slice(1,-1).reduce((x,y)=>x+y,0);
+}
+SumWithoutTheHighestAndTheLowestNumber2([1, 3, 5, 9]);
+console.log(SumWithoutTheHighestAndTheLowestNumber2([1,1, 3, 5, 9]));
